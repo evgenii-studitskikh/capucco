3d732704f9d3b500a62afb81d100dc0e87adf875
Evgeny Studitskikh <estrueall@gmail.com>
2019-06-02 23:42:39 +0400

    server.listen commented

--- server.js
+++ server.js
@@ -48,12 +48,14 @@ app
       return handle(req, res)
     })
 
-    if (dev) {
-      server.listen(3000, err => {
-        if (err) throw err
-        console.log('> Ready on http://localhost:3000')
-      })
-    }
+    server.listen(process.env.PORT || 5000)
+
+    // if (dev) {
+    //   server.listen(3000, err => {
+    //     if (err) throw err
+    //     console.log('> Ready on http://localhost:3000')
+    //   })
+    // }
   })
   .catch(ex => {
     console.error(ex.stack)
