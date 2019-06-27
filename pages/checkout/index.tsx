21f67064b6bc108bed52e92bc12b9044ad39cab3
Evgeny Studitskikh <estrueall@gmail.com>
2019-06-27 22:50:39 +0400

    Checkout page created

---
+++ pages/checkout/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Head from 'next/head';
+import { withRouter } from 'next/router';
+
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+import {
+  Container,
+  Wrapper
+} from '../styled';
+import {
+  Content
+} from './styled';
+
+class Checkout extends React.Component {
+
+  render() {
+
+    return (
+      <Container>
+        <Head>
+          <title>Capucco: checkout</title>
+        </Head>
+        <Header />
+        <Wrapper>
+          <Content>
+            Details form
+          </Content>
+        </Wrapper>
+        <Footer />
+      </Container>
+    )
+  }
+}
+
+export default withRouter(Checkout)
